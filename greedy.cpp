eadfcaf7f28ed250b507c0760fac29140adec623
Notchayan <130827975+Notchayan@users.noreply.github.com>
2023-05-24 00:37:19 +0530

    Update greedy.cpp

--- greedy.cpp
+++ greedy.cpp
@@ -1,1 +1,6 @@
-
+/* Greedy method is used when we have to find the most optimal solution from a set of feasible solutions 
+   when we have different different choices in it 
+   1.Greedy method 
+   2.Dynamic programing 
+   3.Branch and Bound
+*/
